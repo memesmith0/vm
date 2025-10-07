d511cf1574589d909526ecebfc14c1e0342dac9e
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-07 09:11:41 -0700

    Create vm.c
    asdifjaosdijfaosdfj

---
+++ vm.c
@@ -0,0 +1,8 @@
+//vm.c gpl2 john morris beck
+#include <stdio.h>
+#include <stdlib.h>
+#define k(x,y) case x: y;break;
+#define t (r[222]->p)
+#define a r[(t+1)->n]
+#define b r[(t+2)->p]
+typedef struct M{int n;struct M *p;}m;m r[8192],i;r->p=r;t=r[256];i=t;int main(){while(i++->n=getchar());while(t->n!='k'){switch(t->n){k('-',a.n-=b.n;)k('<',a.n=a.n<b.n)k('i',a.n=getchar())k('o',putchar(a.n))k('w',b.p->n=a.p;b.p->p=a.p)k('j',a.p+=a.n)k('m',a.p=malloc(sizeof(m)*a.n))k('f',free(a.p))}t+=3;}}
