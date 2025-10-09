6502a63713498d9ea94866f312949603c1050494
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-09 10:44:50 -0700

    Update vm.c
    asodijfaoifjoiasjf

--- vm.c
+++ vm.c
@@ -9,4 +9,4 @@
 #define f g[d]
 #define w r[c]
 #define o(rb,rc,rd,re,rf,rg,rh,ri,rj,rk,rl,rm)  while(x){c=*(h+1);d=*(h+2);e=*h;j=3;switch(e){k(rb,v=v-f)k(rc,v=v<f)k(rd,h=(int*)w;j=0)k(re,w=(void*)((char*)w+(int)v))k(rf,memcpy(r[d],w,f))k(rg,v=f)k(rh,{w=malloc(f);f=(w==NULL);})k(ri,free(r[d]))k(rj,fread(w, 1, f, stdin))k(rk,fwrite(w, 1, f, stdout))k(rl,fflush(stdout))k(rm,x=0)}h+=j;}
-int g[256],z[256];void* r[256];int main(){int c,d,e,j,i=256,*h=z,*s=h,x=1;while(--i >= 0){g[i]=1;r[i]=&r[1];}i=getc(stdin);while('\n'!=(*s++=getc(stdin)));h=z;switch((i)++){k('r',o('-','<','x','j','b','?','m','f','i','o','.','k'))default:o(0,1,2,3,4,5,6,7,8,9,10,11)}return 0;}
+int g[256],z[256];void* r[256];int main(){int c,d,e,j,i=256,*h=z,*s=h,x=1;while(--i >= 0){g[i]=1;r[i]=&r[1];}i=getc(stdin);while('\n'-(*s++=getc(stdin)));h=z;switch((i)++){k('r',o('-','<','x','j','b','?','m','f','i','o','.','k'))default:o(0,1,2,3,4,5,6,7,8,9,10,11)}return 0;}
