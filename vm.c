d4d083d8ed71046623618c6cd86ae42dcace7d2a
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-09 13:32:31 -0700

    Update vm.c
    sodfjosfjdsiojf

--- vm.c
+++ vm.c
@@ -1,13 +1,10 @@
 //vm.c john morris beck the license to use this code is gpl2 found at gnu.org
-//https://github.com/memesmith0/vm/
-//compile: with gcc -std=c99 -O3 -march=native -flto vm.c -o vm
+//compile with:gcc -std=c99 -O3 -march=native -flto vm.c -o vm
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
+#define d r[a]
+#define e g[a]
+#define f g[b]
 #define k(x,y)case x:y;break;
-#define v g[c]
-#define f g[d]
-#define w r[c]
-#define o(rb,rc,rd,re,rf,rg,rh,ri,rj,rk,rl,rm)while(x){c=*(h+1);d=*(h+2);e=*h;j=3;switch(e){k(rb,v=v-f)k(rc,v=v<f)k(rd,h=(int*)w;j=0)k(re,w=(void*)((char*)w+(int)v))k(rf,memcpy(r[d],w,f))k(rg,v=f)k(rh,{w=malloc(f);f=(w==NULL);})k(ri,free(r[d]))k(rj,fread(w, 1, f, stdin))k(rk,fwrite(w, 1, f, stdout))k(rl,fflush(stdout))k(rm,x=0)}h+=j;}
-int g[256],z[256];void* r[256];int main(){int c,d,e,j,i=256,*h=z,*s=h,x=1;while(--i >= 0){g[i]=1;r[i]=&r[1];}i=getc(stdin);while('\n'-(*s++=getc(stdin)));h=z;switch((i)++){k('r',o('-','<','x','j','b','?','m','f','i','o','.','k'))default:o(0,1,2,3,4,5,6,7,8,9,10,11)}return 0;}
+int i=256,g[i],z[8192],a,b;void* r[i],c;int main(){int *h=z,x=1;while(i--){g[i]=1;r[i]=&r[1];}i=getc(stdin);while('\n'-(z[i++]=getc(stdin)));while(x){a=*(h+1);b=*(h+2);c=*h;switch(c){k(0,e=e-f)k(1,e=e<f)k(2,h=(int*)d)k(3,d=(void*)((char*)d+(int)e))k(4,f=e;r[b]=d)k(5,{r[b]=malloc(e);e=(r[b]==NULL);})k(6,free(d))k(7,fread(d, 1, f, stdin))k(8,fwrite(d, 1, f, stdout))k(9,fflush(stdout))k(10,x=0)}h+=3}}
