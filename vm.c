dfe9d66513527c8289c870de1a4caa567b90758f
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-09 14:04:33 -0700

    Update vm.c
    asdfoijasojasiodfj

--- vm.c
+++ vm.c
@@ -6,5 +6,5 @@
 #define d r[a]
 #define e g[a]
 #define f g[b]
-#define k(x,y)case x:y;break;
-int i=256,g[256],z[8192],a,b,c;void* r[256];int main(){int *h=z,x=1;while(i--){g[i]=1;r[i]=&r[1];}i=getc(stdin);while('\n'-(z[i++]=getc(stdin)));while(x){a=*(h+1);b=*(h+2);c=*h;switch(c){k(0,e=e-f)k(1,e=e<f)k(2,h=(int*)d)k(3,d=(void*)((char*)d+(int)e))k(4,f=e;r[b]=d)k(5,r[b]=malloc(e);e=(r[b]==NULL))k(6,free(d))k(7,fread(d, 1, f, stdin))k(8,fwrite(d, 1, f, stdout))k(9,fflush(stdout))k(10,x=0)}h+=3;}}
+#define k(x,y) case x:y;break;
+int g[256],z[8192],a=0,b,c;void* r[256];int main(){int *h=z,x=1;*g=1;*r=(void*)r;while('\n'-(z[a++]=getc(stdin)));while(x){a=*(h+1);b=*(h+2);c=*h;switch(c){k(0,e=e-f)k(1,e=e<f)k(2,h=(int*)d)k(3,d=(void*)((char*)d+(int)e))k(4,f=e;r[b]=d)k(5,r[b]=malloc(e);e=(r[b]==NULL))k(6,free(d))k(7,fread(d, 1, f, stdin))k(8,fwrite(d, 1, f, stdout))k(9,fflush(stdout))k(10,x=0)}h+=3;}}
