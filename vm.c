1c5149778a59f8f7ced4ed0efe922b842689f99d
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-09 16:51:02 -0700

    Update vm.c
    aosidfjoaisdjfoaisdjf

--- vm.c
+++ vm.c
@@ -6,17 +6,6 @@
 #define x p[a]
 #define y i[a]
 #define z i[b]
+#define v void*
 #define k(x,y) case x:y;break;
-int main(){int r=3,a,b,c,i[256],f[8192],*t=f;void* p[256];*i=1;*p=(void*)p;p[2]=&t;while(11-(t[r++]=getc(stdin)));
-  while(r){t+=3;a=t[1];b=t[2];c=*t;switch(c){
-      k(0,y-=z)
-	k(1,y=y<z)
-	k(2,x=(void*)((char*)(x+(int)y)))
-	k(3,*(int*)p[b]=(int)*(int*)x)
-	k(4,*(void**)p[b]=*(void**)x)
-	k(5,p[b]=malloc(y);y=p[b]==NULL)
-	k(6,free(x))
-	k(7,fread(x,1,z,stdin))
-	k(8,fwrite(x,1,z,stdout))
-	k(9,fflush(stdout))
-	k(10,r=0)}}}
+int main(){int r=3,a,b,c,i[256],f[512],*t=f;v p[256];*i=1;*p=(v)p;p[1]=&t;while(11-(t[r++]=getchar()));while(r){t+=3;a=t[1];b=t[2];c=*t;switch(c){k(0,y-=z)k(1,y=y<z)k(2,x=(v)((char*)(x+(int)y)))k(3,*(int*)p[b]=(int)*(int*)x)k(4,*(v*)p[b]=*(v*)x)k(5,p[b]=malloc(y);y=p[b]==NULL)k(6,free(x))k(7,fread(x,1,z,stdin))k(8,fwrite(x,1,z,stdout))k(9,fflush(stdout))k(10,r=0)}}}
