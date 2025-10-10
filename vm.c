92be1acd5eeef246f9ada44bae06b2db5ad62f00
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-10 07:44:06 -0700

    Update vm.c
    the forth programmers said I only needed 16 registers

--- vm.c
+++ vm.c
@@ -8,4 +8,4 @@
 #define z i[b]
 #define v void*
 #define k(x,y) case x:y;break;
-int main(){int r=3,a,b,c,i[16],f[512],*t=f;v p[16];*i=1;*p=(v)p;p[1]=&t;while(11-(t[r++]=getchar()));while(r){t+=3;a=t[1];b=t[2];c=*t;switch(c){k(0,y-=z)k(1,y=y<z)k(2,x=(v)((char*)(x+(int)y)))k(3,*(int*)p[b]=(int)*(int*)x)k(4,*(v*)p[b]=*(v*)x)k(5,y?p[b]=malloc(y):free(x);y=(p[b]==NULL))k(6,fread(x,1,z,stdin))k(7,fwrite(x,1,z,stdout);fflush(stdout))k(8,r=0)}}}
+int main(){int r=3,a,b,c,i[16],f[512],*t=f;v p[16];*i=1;*p=(v)p;p[1]=&t;while(16-(t[r++]=getchar()));while(r){t+=3;a=t[1];b=t[2];c=*t;switch(c){k(0,y-=z)k(1,y=y<z)k(2,x=(v)((char*)(x+(int)y)))k(3,memcpy(p[b],x,y))k(4,y?p[b]=malloc(y):free(x);y=(p[b]==NULL))k(5,fread(x,1,z,stdin))k(6,fwrite(x,1,z,stdout);fflush(stdout))k(7,r=0)}}}
