3efcf59c6e5315ff3e0c7d95f232d55740c5443b
memesmith0 <memesmith.kestrel@gmail.com>
2025-10-10 09:59:24 -0700

    Update vm.c
    asdfoijsdoijdsofj

--- vm.c
+++ vm.c
@@ -2,9 +2,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define w p[b]
-#define x p[a]
-#define y i[a]
-#define z i[b]
-#define k(x,y) case x:y;break;
-int main(){int a=0,b,c,i[16]={1},f[512],*t=f;void* p[16]={&t};while(16-(t[a++]=getchar()));while(1){a=t[1];b=t[2];c=*t;switch(c){k(0,y-=z)k(1,y<=z)k(3,w=x)k(4,z=y)k(5,w=*(void**)x)k(6,*(void**)w=x)k(7,x=(void*)((char*)(x+(int)y)))k(8,y?w=malloc(y):free(x);y=(w==NULL))k(9,fread(x,1,z,stdin))k(10,fwrite(x,1,z,stdout);fflush(stdout))k(11,exit(0))}t+=3;}}
+int main(){int x=0,y=16,z=0,i[16],f[512],*t=f;void* p[16];while(y--){i[y]=1;p[y]=&t;}for(;x<512;t[x++]=getchar());while(1){x=t[1];y=t[2];z=*t;switch(z){case 0:i[x]-=i[y];break;case 1:i[x]=i[x]<i[y];break;case 3:p[y]=p[x];break;case 4:i[y]=i[x];break;case 5:p[y]=*(void**)p[x];break;case 6:*(void**)p[y]=p[x];break;case 7:p[y]=(void*)((char*)(p[y]+(int)i[x]));break;case 8:i[x]?p[y]=malloc(i[x]):free(p[y]);i[x]=(p[y]==NULL);break;case 9:fread(p[y],1,i[y],stdin);break;case 10:fwrite(p[y],1,i[y],stdout);fflush(stdout);break;case 11:exit(0);}t+=3;}}
